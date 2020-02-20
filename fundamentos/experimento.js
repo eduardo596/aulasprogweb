eab327dba3f43cb98c44aab85a2ce411c23743ee
Eduardo Aureliano <eduardoaureliano96@outlook.com>
2020-02-20 19:16:37 -0300

    atualizando.

---
+++ fundamentos/experimento.js
@@ -0,0 +1,19 @@
+let a = 3
+
+global.b = 123
+this.c = 456
+this.d = false
+this.e = 'teste'
+
+console.log(this.a)
+console.log(global.b)
+console.log(this.c)
+console.log(module.exports.c)
+console.log(module.exports === this)
+console.log(module.exports)
+
+// criando um variavel maluca sem var e let!
+abc = 3 // nao fazer isso, dont do this
+console.log (global.abc)
+
+// module.exports = { c: 456, d: false, e: 'teste' }
